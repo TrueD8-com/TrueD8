69fe5737214e00ad5444e66ef684bcd78bb7b937
mahdieh-amiri1 <mahdieh.amiri.it@gmail.com>
2025-10-26 15:11:29 +0330

    Update hardhat config

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,7 +1,10 @@
 import type { HardhatUserConfig } from "hardhat/config";
-
 import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomicfoundation/hardhat-etherscan"; // for contract verification
+import * as dotenv from "dotenv";
 import { configVariable } from "hardhat/config";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   plugins: [hardhatToolboxViemPlugin],
@@ -35,6 +38,25 @@ const config: HardhatUserConfig = {
       chainType: "l1",
       url: configVariable("SEPOLIA_RPC_URL"),
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
+    hederaTestnet: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("HEDERA_TESTNET_RPC"),
+      accounts: [configVariable("HEDERA_TESTNET_PRIVATE_KEY")],
+    },
+    hederaMainnet: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("HEDERA_MAINNET_RPC"),
+      accounts: [configVariable("HEDERA_MAINNET_PRIVATE_KEY")],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY || "",
+      hederaTestnet: process.env.HEDERA_EXPLORER_API_KEY || "",
+      hederaMainnet: process.env.HEDERA_EXPLORER_API_KEY || "",
     },
   },
 };
